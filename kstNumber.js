6a004a9a257e6a81afd91b87ca6e733900fe483a
Hachu.developer <65321592+hachuu@users.noreply.github.com>
2020-10-22 14:13:36 +0900

    Add files via upload
    코딩테스트 연습
    정렬
    K번째수

---
+++ kstNumber.js
@@ -0,0 +1,59 @@
+/* 문제
+배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+2에서 나온 배열의 3번째 숫자는 5입니다.
+배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+array의 길이는 1 이상 100 이하입니다.
+array의 각 원소는 1 이상 100 이하입니다.
+commands의 길이는 1 이상 50 이하입니다.
+commands의 각 원소는 길이가 3입니다.
+입출력 예
+array   commands    return
+[1, 5, 2, 6, 3, 7, 4]   [[2, 5, 3], [4, 4, 1], [1, 7, 3]]   [5, 6, 3]
+입출력 예 설명
+[1, 5, 2, 6, 3, 7, 4]를 2번째부터 5번째까지 자른 후 정렬합니다. [2, 3, 5, 6]의 세 번째 숫자는 5입니다.
+[1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
+[1, 5, 2, 6, 3, 7, 4]를 1번째부터 7번째까지 자릅니다. [1, 2, 3, 4, 5, 6, 7]의 세 번째 숫자는 3입니다.
+*/
+
+// 내가 한 것
+function solution(array, commands) {
+    let answer = [];
+    
+    commands.map((x,i) => {
+        let newArray = [];
+        
+        for(let i=x[0]-1; i<x[1]; i++) {
+            newArray.push(array[i]);
+        }
+        newArray = newArray.sort((a,b)=>a-b); //sort()만 했다가 문제2에서 실패가 남 : sort((a,b)=>a-b)처리를 추가로 해줌
+        answer.push(newArray[(x[2]-1)]);
+    })
+    return answer;
+}
+
+// 다른 사람이 제출한 소스
+// slice를 통해 바로 array를 잘라 직관적으로 값을 가져옴
+function solution(array, commands) {
+    return commands.map(v => {
+        return array.slice(v[0] - 1, v[1]).sort((a, b) => a - b).slice(v[2] - 1, v[2])[0];
+    });
+}
+
+// const [sPosition, ePosition, position] = command => 내가 생각해내지 못한 방법
+function solution(array, commands) {
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command
+        const newArray = array
+            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+            .sort((a,b) => a - b)    
+
+        return newArray[position - 1]
+    })
+}
